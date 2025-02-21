5ee4b9d6842a76de6fe238f62126f09efd4578ac
Niriza Lailaiumi Hidayat <nirizalailaumi1@gmail.com>
2025-02-21 17:00:02 +0700

    Praktikum Route

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,35 @@ Route::get('/', function () { //mendaftarkan route GET / (halaman utama)
     return view('welcome'); //menampilkan tampilan welcome.blade.php
 });
 
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'NIM: 2341760072, Nama: Niriza Lailaumi Hidayat';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' .$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' .$name;
+});
 Route::resource('items', ItemController::class); //mendaftarkan semua route CRUD (index, create, store, show, edit, update, destroy) untuk ItemController, menghubungkan endpoint /items dengan metode yang ada di ItemController
